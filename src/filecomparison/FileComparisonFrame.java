0d41c829c593b01b80928806d7dbc48d2bafb682
Gena Koganovich <genakoganovich@gmail.com>
2017-02-20 23:12:36 +0200

    0013: mute added

--- src/filecomparison/FileComparisonFrame.java
+++ src/filecomparison/FileComparisonFrame.java
@@ -2,6 +2,7 @@ package filecomparison;
 
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 class FileComparisonFrame extends JFrame
 {
@@ -10,6 +11,31 @@ class FileComparisonFrame extends JFrame
         JPanel fileExplorerPanel = new FileExplorerPanel();
         fileExplorerPanel.setLayout(new BoxLayout(fileExplorerPanel, BoxLayout.LINE_AXIS));
         add(fileExplorerPanel);
+
+        JMenu testMenu = new JMenu("Test");
+        testMenu.add(new TestAction("Adaptive subtraction"));
+        testMenu.add(new TestAction("Despike"));
+        testMenu.add(new TestAction("LNA"));
+        testMenu.add(new TestAction("gn3160"));
+        testMenu.add(new TestAction("Radon MA"));
+        testMenu.add(new TestAction("Radon TauP"));
+
+
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        menuBar.add(testMenu);
         pack();
+    }
+    class TestAction extends AbstractAction
+    {
+        public TestAction(String name)
+        {
+            super(name);
+        }
+
+        public void actionPerformed(ActionEvent event)
+        {
+            System.out.println(getValue(Action.NAME) + " selected.");
+        }
     }
 }
